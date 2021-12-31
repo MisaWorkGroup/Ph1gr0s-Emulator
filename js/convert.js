9e7a636cb64eea37e4364112e1a0aebb07481930
HIMlaoS_Misa <62408957+MisaLiu@users.noreply.github.com>
2021-12-31 09:44:56 +0800

    Add files via upload

--- js/convert.js
+++ js/convert.js
@@ -603,6 +603,50 @@ function ConvertPEC2Json(pec, filename) {
 }
 
 
+function ConvertChartVersion(chart) {
+	let newChart = JSON.parse(JSON.stringify(chart));
+	
+	switch (newChart.formatVersion) {
+		case 1: {
+			newChart.formatVersion = 3;
+			for (const i of newChart.judgeLineList) {
+				let floorPosition = 0;
+				
+				for (const x of i.speedEvents) {
+					if (x.startTime < 0) x.startTime = 0;
+					x.floorPosition = y;
+					floorPosition += (x.endTime - x.startTime) * x.value / i.bpm * 1.875;
+				}
+				
+				for (const x of i.judgeLineDisappearEvents) {
+					x.start2 = 0;
+					x.end2   = 0;
+				}
+				
+				for (const x of i.judgeLineMoveEvents) {
+					x.start2 = x.start % 1e3 / 520;
+					x.end2   = x.end % 1e3 / 520;
+					x.start  = parseInt(x.start / 1e3) / 880;
+					x.end    = parseInt(x.end / 1e3) / 880;
+				}
+				
+				for (const x of i.judgeLineRotateEvents) {
+					x.start2 = 0;
+					x.end2   = 0;
+				}
+			}
+		}
+		case 3: { }
+		case 3473:
+			break;
+		default:
+			throw '谱面数据错误：未知的谱面格式版本：' + newChart.formatVersion;
+	}
+	
+	return newChart;
+}
+
+
 function Csv2Array(data, isObject) {
 	const strarr = data.replace(/\r/g, "").split("\n");
 	const col = [];
